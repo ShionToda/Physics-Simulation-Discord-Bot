5ed96fd8c3b6e66d5cc228b30f70c82982ada23b
Shion <shion.toda@gmail.com>
2021-02-06 18:46:52 -0800

    Gradle and Main Class Initialization
    -Imported PostgreSQL and Discord4j from Graadle.
    -Made the basic set up for the Main Class

---
+++ PhysicsSimulationDiscordBot/src/main/java/Main.java
@@ -0,0 +1,51 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.Channel;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.object.entity.channel.TextChannel;
+
+public class Main {
+
+    public static void main (String[] arguments) {
+
+        // Shortcut to new DiscordClientBuilder(token).build()
+        DiscordClient client = DiscordClient.create(System.getenv(arguments[0]));
+        // Acquire a synchronous handle on gateway connections, can be used to register events, get cached entities, etc.
+        GatewayDiscordClient gateway = client.login().block();
+
+        // Shortcut to gateway.getEventDispatcher().on(ReadyEvent.class)
+        gateway.on(ReadyEvent.class)
+                .subscribe(ready ->
+                        System.out.println("Logged in as " + ready.getSelf().getUsername())
+                );
+
+        // add database initialization
+
+
+        // add input handling initialization
+
+
+        if (gateway == null) throw new AssertionError();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+
+            final Message message = event.getMessage();
+            final MessageChannel channel = message.getChannel().block();
+
+            // database
+
+
+            // PROCESSES COMMANDS
+
+            
+        });
+
+        // To keep the main thread alive, await until bot disconnects through logout()
+        gateway.onDisconnect().block();
+    }
+
+}
