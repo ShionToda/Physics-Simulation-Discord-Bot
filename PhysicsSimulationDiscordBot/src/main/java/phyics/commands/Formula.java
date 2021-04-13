f016a06a3a8a2ba4bf0d066ce5062b11f4882594
tw12ce <48884467+tw12ce@users.noreply.github.com>
2021-04-12 21:06:03 -0700

    g

--- PhysicsSimulationDiscordBot/src/main/java/phyics/commands/Formula.java
+++ PhysicsSimulationDiscordBot/src/main/java/phyics/commands/Formula.java
@@ -2,8 +2,17 @@ package phyics.commands;
 
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.Embed;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.function.Consumer;
 
 public class Formula extends Command {
     private String queuedUserID;
@@ -31,7 +40,18 @@ public class Formula extends Command {
             else if (content.equalsIgnoreCase("kinematics")){
                 channel.createMessage("Branch? kinematics + (1D, 2D)").block();
             } else if (content.equalsIgnoreCase("kinematics1d")){
-                channel.createMessage("(insert all formulas here)").block();
+                try {
+                    channel.createEmbed(new Consumer<EmbedCreateSpec>() {
+                        @Override
+                        public void accept(EmbedCreateSpec embedCreateSpec) {
+                            embedCreateSpec.setImage("https://cdn.discordapp.com/attachments/699070356165754912/831371860540129321/BO82YZEm2LGnHiU5RcqaKRltWAkf4MTXv-QcUCe09uVP2h-2FSWaYTzTtEHxhD2-sehTRstmwW1MdPpTI5aIKC4.png").asRequest();
+                        }
+                    }).block();
+                }catch(Exception e) {
+                    channel.createMessage("Error!").block();
+                    e.printStackTrace();
+                    System.exit(1);
+                }
             }
         }
     }
